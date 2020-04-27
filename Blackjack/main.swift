a90224ca07c4c7a6a0a50c19bdd6723d7561a676
mwielondek <miloszw@kth.se>
2020-04-27 15:57:16 +0200

    Initial Commit

---
+++ Blackjack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Blackjack
+//
+//  Created by Milosz Wielondek on 27/04/2020.
+//  Copyright © 2020 Seeking Clarity. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
